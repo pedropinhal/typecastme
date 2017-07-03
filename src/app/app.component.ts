4569fdc20fc1b985ad730940e35ad54e651b0bb0
Pedro <pedro>
2017-07-03 22:58:28 +0100

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,33 +3,16 @@ import {
   OnInit
 } from '@angular/core';
 
-import {
-  HeroService
-} from './hero.service';
-import {
-  Hero
-} from './hero';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
 
 export class AppComponent implements OnInit {
-  constructor(private heroService: HeroService) {}
+  constructor() {}
   ngOnInit() {
-    this.getHeroes();
   }
 
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
