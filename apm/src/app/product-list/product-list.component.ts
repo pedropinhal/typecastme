4c8e7c29bc076f01fb92e1c980f79c0c6dd8a7df
Pedro <pedro>
2017-10-23 20:03:39 +0100

    filter

--- apm/src/app/product-list/product-list.component.ts
+++ apm/src/app/product-list/product-list.component.ts
@@ -1,32 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { IProduct } from './product'
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  IProduct
+} from './product'
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
+
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
   showImage: boolean = false;
   pageTitle: string = 'Product List';
-  listFilter: string = 'cart';
-  products: IProduct[] =[{
+  _listFilter: string;
+  filteredProducts: IProduct[];
+  products: IProduct[] = [{
     "productId": 2,
     "productName": "Garden Cart",
-    "productCode": "GDNCart",
+    "productCode": "GDN-Cart",
     "releaseDate": "18 March, 2017",
     "price": 32.99,
     "starRating": 4.2,
     "imageUrl": "http://lorempixel.com/200/200/"
+  },{
+    "productId": 3,
+    "productName": "Shovel",
+    "productCode": "GDN-shovel",
+    "releaseDate": "27 November, 2019",
+    "price": 13.99,
+    "starRating": 3.7,
+    "imageUrl": "http://lorempixel.com/200/200/"
   }]
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.filter(this.listFilter) : this.products;
+  }
+
+  filter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) != -1
+    );
+  }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
+  ngOnInit() {
+    console.log("init");
+  }
 }
