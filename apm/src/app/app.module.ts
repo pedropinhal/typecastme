6e116b3e06384e14da24575ce450854f85b7b41e
Pedro <pedro>
2017-11-06 20:23:19 +0000

    module

--- apm/src/app/app.module.ts
+++ apm/src/app/app.module.ts
@@ -4,19 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
 import { StarComponent } from './star/star.component';
 import { ProductService } from './services/product.service';
-import { ProductDetailComponentComponent } from './products/product-detail-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponentComponent
+    StarComponent
   ],
   imports: [
     BrowserModule,
