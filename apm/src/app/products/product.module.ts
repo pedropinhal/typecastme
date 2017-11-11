be23175255e9b982f02952f4841097d382237783
Pedro <pedro>
2017-11-11 20:26:42 +0000

    shared

--- apm/src/app/products/product.module.ts
+++ apm/src/app/products/product.module.ts
@@ -1,30 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../star/star.component';
 import { ConvertToSpacesPipe } from '../pipes/convert-to-spaces.pipe';
 import { ProductListComponent } from './product-list.component';
 import { RouterModule } from '@angular/router';
 import { ProductGuardService } from './product-guard.service';
 import { ProductService } from '../services/product.service';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'product/:id', 
         component: ProductDetailComponent,
         canActivate: [ProductGuardService] },
       {path: '*', redirectTo: 'products', pathMatch: 'full'}
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent,
     ProductDetailComponent
   ],
   providers: [ProductService, ProductGuardService],
