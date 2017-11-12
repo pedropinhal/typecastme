ef8b0b3f96723aade4efe2e88c0c551ef6825b5e
Pedro <pedro>
2017-11-12 15:35:14 +0000

    starting again

--- apm/src/app/products/product.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct {
-  productId: number;
-  productName: string;
-  productCode: string;
-  releaseDate: string;
-  price: number;
-  starRating: number;
-  imageUrl: string;
-}
