4eecc49a37360d61f041b629c57698b85b73291c
Pedro <pedro>
2017-11-15 21:50:49 +0000

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  spotlightViewPin = '';
+  constructor(private http: HttpClient){
+
+  }
+  // https://www.spotlight.com/8014-6727-0395
+// https://www.spotlight.com/photo/~540x540/ffffff/loose/1/M153065.0103.jpg
+  onSubmit() {
+    this.http.get('https://www.spotlight.com/8014-6727-0395').subscribe(data => {
+      // Read the result field from the JSON response.
+      console.log(data);
+    });
+    console.log(this.spotlightViewPin);
+  }
 }
